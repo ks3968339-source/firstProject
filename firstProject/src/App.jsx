6454704d78c66f52fa4a8f88b5d56bf56c4266da
kushal sharma <ks3968339@gmail.com>
2026-06-18 18:15:21 +0530

    first commit

---
+++ firstProject/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <div className="card">
+        <h1>Hi my name is Kushal</h1>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Amet, maxime.</p>
+      </div>
+    </div>
+  )
+}
+
+export default App
+
