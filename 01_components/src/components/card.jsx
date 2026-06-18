4a7a2daaea4d57ead5f9a910cc121d9f4ae7dfb1
kushal sharma <ks3968339@gmail.com>
2026-06-18 19:03:56 +0530

    made some changes

---
+++ 01_components/src/components/card.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const card = () => {
+  return (
+    <div>
+      <div className="card">
+        <h1>Kushal sharma  </h1>
+        <pre>.    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aut, repellat!</pre>
+      </div>
+    </div>
+  )
+}
+
+export default card
