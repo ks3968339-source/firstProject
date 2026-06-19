919a7832eb2c64af033e2596300796cf52d5e0d7
kushal sharma <ks3968339@gmail.com>
2026-06-19 19:03:33 +0530

    learned props

---
+++ 02_props/src/components/Card.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Card = (props) => {
+    console.log(props);
+    
+  return (
+    <div>
+      <div className="card">
+        <img src={props.img}></img>
+        <h1>{props.user}</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+        <button>view profile</button>
+      </div>
+    </div>
+  )
+}
+
+export default Card
